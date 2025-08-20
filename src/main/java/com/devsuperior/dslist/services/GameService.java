7d7df8c427c205ec4a49243c410eefcee19b1bce
Eduardo Ceretta <duduceretta@gmail.com>
2025-08-20 13:09:17 -0300

    Get lists

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,19 +3,18 @@ package com.devsuperior.dslist.services;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
-import com.devsuperior.dslist.repositories.GameRpository;
+import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class GameService {
 
     @Autowired
-    private GameRpository gameRpository;
+    private GameRepository gameRpository;
 
     @Transactional(readOnly = true)
     public GameDTO findById(long id) {
